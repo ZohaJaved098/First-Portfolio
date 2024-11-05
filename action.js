b16c9e11025e606167c012cbd66b2a6766c9e90b
Zoha Javed <zohajaved.098@gmail.com>
2024-11-06 00:15:30 +0500

    Initial Commit

---
+++ action.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+const timelineItems = document.querySelectorAll('.timeline-item');
+const handleScroll = () => {
+    timelineItems.forEach(item => {
+        const rect = item.getBoundingClientRect();
+        if (rect.top < window.innerHeight - 100) {
+            item.classList.add('visible');
+        } else {
+            item.classList.remove('visible');
+        }
+    });
+};
+window.addEventListener('scroll', handleScroll);
+handleScroll();
+});
